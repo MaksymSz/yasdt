7e9d31c559d2371e18000850fa7421d2f335d04f
MaksymSz <szemermaksym@student.agh.edu.pl>
2024-05-23 21:03:39 +0200

    README.md

---
+++ yasdt/utils.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot(functions, arg_min, arg_max, show_grid=False, *args, **kwargs):
+    for fn in functions:
+        X = np.linspace(arg_min, arg_max, 200)
+        y = [fn.eval(x) for x in X]
+        plt.plot(X, y, label=str(fn), *args, **kwargs)
+    if show_grid:
+        plt.grid()
+    plt.legend()
+    plt.show()
